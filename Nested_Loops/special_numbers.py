543b8fd515663de47c954fb117014cd0d5739046
Georgi Tashkov <gtashkov94@gmail.com>
2020-12-11 11:49:24 +0200

    Some more while loop exercises

--- Nested_Loops/special_numbers.py
+++ Nested_Loops/special_numbers.py
@@ -1,10 +1,10 @@
 number = int(input())
-is_special_number = True
 
 for i in range(1111, 10000):
+    is_special_number = True
     for index, value in enumerate(str(i)):
-        if int(value) != 0 and number % int(value) != 0:
+        if int(value) != 0 and number % int(value) != 0 or int(value) == 0:
             is_special_number = False
             break
-        if is_special_number:
-            print(int(index), end=" ")
+    if is_special_number:
+        print(int(i), end=" ")
