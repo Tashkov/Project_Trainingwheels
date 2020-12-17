df5e1f6b5078aa88bbe16627970ccec15b34728b
Georgi Tashkov <gtashkov94@gmail.com>
2020-12-17 10:22:38 +0200

    Additional while loop exercises

--- Python_Basics/while_loops/more_exercies/stream_of_letters.py
+++ Python_Basics/while_loops/more_exercies/stream_of_letters.py
@@ -1,5 +1,5 @@
 import string
-line = input()
+line = input()  # The user must input only letters from the latin alphabet
 
 secret_word = "con"
 secret_counter = 0
@@ -10,7 +10,7 @@ while line != "End":
     if line in secret_word:
         secret_counter += 1
         secret_word = secret_word.replace(line,"")
-        if secret_counter % 3 == 0:
+        if secret_counter % 3 == 0:  # the logic for the secret word
             word += temp_word
             word += " "
             secret_word = "con"
@@ -18,7 +18,7 @@ while line != "End":
             line = input()
         else:
             line = input()
-    elif line not in string.ascii_letters:
+    elif line not in string.ascii_letters:  # checks whether there is a symbol
         line = input()
     else:
         temp_word += line
