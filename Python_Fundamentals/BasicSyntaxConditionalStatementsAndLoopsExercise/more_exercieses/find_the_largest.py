105f80804bc2381e29a27abfa5a9c8ff0f31df23
Georgi Tashkov <gtashkov94@gmail.com>
2021-01-15 14:16:42 +0200

    Some extra fundamentals exercises

--- Python_Fundamentals/BasicSyntaxConditionalStatementsAndLoopsExercise/more_exercieses/find_the_largest.py
+++ Python_Fundamentals/BasicSyntaxConditionalStatementsAndLoopsExercise/more_exercieses/find_the_largest.py
@@ -1,10 +1,18 @@
+from itertools import permutations
 number = input()
-combo_number = 1
 
-list = []
-new_number = 0
+list_of_numbers = []
+list_of_ints = []
 
-for i in range(1, len(number) + 1):
-    combo_number *= i
+# takes the input str of number and inserts each integer as a different index in the list_of_numbers
+for i in range(len(number)):
+    list_of_numbers.append(int(number[i]))
 
-while int(number) < new_number:
+perm = permutations(list_of_numbers)
+for i in perm:
+    list_of_ints.append(i)
+    print(i)
+print(list_of_ints)
+
+#You`re half way there. Find out how to convert the indexes from each tuple into an integer
+
--- Python_Fundamentals/BasicSyntaxConditionalStatementsAndLoopsExercise/more_exercieses/wolf_sheep.py
+++
@@ -1,8 +0,0 @@
-animal = input()
-
-herd_list = []
-
-herd_list.append(animal)
-
-for i in range(len(herd_list)):
-    if
