f4676742baaf269f7d4efb94a547e792d7a242cb
Georgi Tashkov <gtashkov94@gmail.com>
2021-01-16 11:18:50 +0200

    Some extra fundamentals exercises

--- Python_Fundamentals/BasicSyntaxConditionalStatementsAndLoopsExercise/christmas_spirit.py
+++ Python_Fundamentals/BasicSyntaxConditionalStatementsAndLoopsExercise/christmas_spirit.py
@@ -5,10 +5,13 @@ ornament_set = 2
 tree_skirt = 5
 tree_garland = 3
 tree_lights = 15
+
 budget = 0
 spirit = 0
 
 for i in range(1, days + 1):
+    if i % 11 == 0:
+        quantity += 2
     if i % 2 == 0:
         budget += ornament_set * quantity
         spirit += 5
@@ -17,16 +20,16 @@ for i in range(1, days + 1):
         spirit += 13
     if i % 5 == 0:
         budget += tree_lights * quantity
+        spirit += 17
         if i % 3 == 0:
-            spirit += 47
-        else:
-            spirit += 17
+            spirit += 30
+
     if i % 10 == 0:
         spirit -= 20
-        budget += (tree_skirt + tree_garland + tree_lights) * 1
-        if i == days:
+        budget += tree_skirt + tree_garland + tree_lights
+
+        if i == days and i % 10 == 0:
             spirit -= 30
-    if i % 11 == 0:
-        quantity += 2
+
 
 print(f"Total cost: {budget}\nTotal spirit: {spirit}")
