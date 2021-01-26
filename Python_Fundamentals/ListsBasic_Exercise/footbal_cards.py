590c77cdafb0760b04535547e985576c021bb181
Georgi Tashkov <gtashkov94@gmail.com>
2021-01-26 10:31:19 +0200

    lml
    lml

--- Python_Fundamentals/ListsBasic_Exercise/footbal_cards.py
+++ Python_Fundamentals/ListsBasic_Exercise/footbal_cards.py
@@ -1,19 +1,25 @@
 card = input()
-the_card_list = list(set(card.split()))
-team_A_counter = 11
-team_B_counter = 11
+the_card_list = list(card.split())
+team_A = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
+team_B = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
 
 for i in the_card_list:
     if "A" in i:
-        team_A_counter -= 1
-    elif "B" in i:
-        team_B_counter -= 1
+        if i in team_A:
+            team_A.remove(i)
+            if len(team_A) < 7 or len(team_B) < 7:
+                break
 
-if team_A_counter < 7 or team_B_counter < 7:
-    print(f"Team A - {team_A_counter}; Team B - {team_B_counter}")
+    elif "B" in i:
+        if i in team_B:
+            team_B.remove(i)
+            if len(team_A) < 7 or len(team_B) < 7:
+                break
+
+if len(team_A) < 7 or len(team_B) < 7:
+    print(f"Team A - {len(team_A)}; Team B - {len(team_B)}")
     print("Game was terminated")
     exit()
 else:
-    print(f"Team A - {team_A_counter}; Team B - {team_B_counter}")
+    print(f"Team A - {len(team_A)}; Team B - {len(team_B)}")
 
-# Make it work 100/100 now its 80/100
---
+++ Python_Fundamentals/ListsBasic_Exercise/number_beggars.py
@@ -0,0 +1,11 @@
+total_offers = input()
+total_beggars = input()
+
+offers_list = list(total_offers)
+beggars = int(total_beggars)
+
+for i in range(beggars):
+
+
+print(offers_list)
+print(beggars)
